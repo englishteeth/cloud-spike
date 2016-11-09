44ef57b53ce88d8ffba65ac9b67a2673526dc644
Ian Robinson <ian@englishteeth.com>
2016-11-09 15:47:07 +0200

    Docker plus local
    Changes to configuration to allow for running services locally or in
    docker containers

--- cluster-configuration/src/main/java/service/Application.java
+++ cluster-configuration/src/main/java/service/Application.java
@@ -2,16 +2,16 @@ package service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @EnableAutoConfiguration
-@EnableDiscoveryClient
 @EnableConfigServer
-public class Application {
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+public class Application
+{
+  public static void main(String[] args)
+  {
+    SpringApplication.run(Application.class, args);
+  }
 }
